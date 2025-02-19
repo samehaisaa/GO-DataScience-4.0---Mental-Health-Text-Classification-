a0500ddf018634011681c8eaaae2cf42163eeb1e
samehaisaa <a.semeh555@gmail.com>
2025-02-19 15:30:05 +0000

    Add WeightedTrainer class for custom loss computation in model training

--- src/models/model.py
+++ src/models/model.py
@@ -2,6 +2,7 @@ import os
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
+from transformers import Trainer
 class MentalHealthDataset(Dataset):
     def __init__(self, texts, labels=None, cache_dir='./cache', tokenizer=None):
         self.texts = texts.tolist() if isinstance(texts, pd.Series) else texts
@@ -35,3 +36,13 @@ class MentalHealthDataset(Dataset):
 
     def __len__(self):
         return len(self.encodings['input_ids'])
+
+class WeightedTrainer(Trainer):
+    def compute_loss(self, model, inputs, class_weights, return_outputs=False):
+        labels = inputs.get("labels")
+        outputs = model(inputs)
+        logits = outputs.get("logits")
+        loss_fct = torch.nn.CrossEntropyLoss(weight=class_weights)
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
+
